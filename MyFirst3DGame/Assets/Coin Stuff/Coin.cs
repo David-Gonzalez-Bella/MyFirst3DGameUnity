7925637f7e11a45e661aeed70675d1a5266fbeab
DavidGonzalezBella <davidxxkie@gmail.com>
2020-04-12 22:03:26 +0200

    SandboxCoinsUnity
    He subido todo el jugo de golpe a modo de prueba para vincular Unity y GitHub

---
+++ MyFirst3DGame/Assets/Coin Stuff/Coin.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    public static int coinCount = 0;
+        
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinCount++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") == true)
+        {
+            coinCount--;
+            Debug.Log("Quedan " + coinCount + " monedas por recolectar.");
+            Destroy(gameObject);
+
+            if(coinCount == 0)
+            {
+                Debug.Log("Enhorabuena, has recogido todas las monedas!!");
+
+                GameObject gameManager = GameObject.Find("GameManager");
+                Destroy(gameManager);
+
+                GameObject [] fireworksSystem = GameObject.FindGameObjectsWithTag("Fireworks");
+                foreach (GameObject firework in fireworksSystem)
+                {
+                    firework.GetComponent<ParticleSystem>().Play();
+                }
+            }
+        }
+    }
+}
---
+++ MyFirst3DGame/Assets/Coin Stuff/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+
+    public float maxTime = 90.0f; //La pongo publica por si la queremos cambiar en el interfaz
+
+    private float countdown = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        Debug.Log(countdown);
+
+        if (countdown <= 0.0f)
+        {
+            Debug.Log("Fin del tiempo!!");
+
+            Coin.coinCount = 0;
+            SceneManager.LoadScene("SampleScene"); // ó SceneManager.LoadScene(0);, para ver las sobrecargas dale, con el cursor dentro del parentresis, a "Ctrl + Shft + Espacio"
+
+        }
+    }
+}
